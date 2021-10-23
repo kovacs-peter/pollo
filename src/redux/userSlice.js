d63ff29c838ab58e4e6b27c0c704a80124e42dbb
peterkovacs <koovacspeter@gmail.com>
2021-10-23 15:50:09 +0200

    debugger

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -7,7 +7,6 @@ export const userSlice = createSlice({
     initialState,
     reducers: {
         setUser: (state, action) => {
-            debugger;
             state.userData = action.payload;
         },
     },

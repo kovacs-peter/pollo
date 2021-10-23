1d392e91c5be71b584c8faaed8604477822572be
peterkovacs <koovacspeter@gmail.com>
2021-10-23 20:57:18 +0200

    login with session

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,17 +3,21 @@ import { auth } from "../api/firebase";
 import { GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "firebase/auth";
 import { setUser } from "../redux/userSlice";
 import { useDispatch } from "react-redux";
-
+import { useHistory } from "react-router";
 const provider = new GoogleAuthProvider(auth);
 
 const Login = () => {
     const dispatch = useDispatch();
+    const history = useHistory();
 
     useEffect(() => {
         getRedirectResult(auth)
             .then((result) => {
+                sessionStorage.setItem("user", JSON.stringify(result.user));
                 dispatch(setUser(result.user));
+                history.push("/");
             })
+
             .catch((error) => {
                 console.log(error.code);
             });
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -5,26 +5,40 @@ import { Switch, Route } from "react-router-dom";
 import PollConfig from "./edit-poll/poll-config";
 import PollList from "./my-polls/poll-list";
 import PollFill from "./fill-out-poll/poll-fill";
+import { useEffect } from "react";
+import { setUser } from "../redux/userSlice";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const Routes = () => {
     const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const history = useHistory();
+    useEffect(() => {
+        const userJSON = sessionStorage.getItem("user");
+        if (userJSON) {
+            const parsedUser = JSON.parse(userJSON);
+            dispatch(setUser(parsedUser));
+        } else {
+            history.push("/login");
+        }
+    }, [dispatch, history]);
 
-    if (user)
-        return (
-            <Layout>
-                <Switch>
-                    <Route path={"/:id(\\d+)"} exact strict component={PollFill} />
-                    <Route path={"/edit/:id(\\d+)"} exact strict>
-                        <PollConfig action="edit" />
-                    </Route>
-                    <Route path="/new" exact strict>
-                        <PollConfig action="new" />
-                    </Route>
-                    <Route path={"/"} exact strict component={PollList} />
-                </Switch>
-            </Layout>
-        );
-    return <Login />;
+    return (
+        <Layout>
+            <Switch>
+                <Route path={"/:id(\\d+)"} exact strict component={PollFill} />
+                <Route path={"/edit/:id(\\d+)"} exact strict>
+                    <PollConfig action="edit" />
+                </Route>
+                <Route path="/new" exact strict>
+                    <PollConfig action="new" />
+                </Route>
+                <Route path={"/login"} exact strict component={Login} />
+                <Route path={"/"} exact strict component={PollList} />
+            </Switch>
+        </Layout>
+    );
 };
 
 export default Routes;

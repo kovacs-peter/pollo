4c3c996b63eda822492bca9d96cea801a7ab3e78
peterkovacs <koovacspeter@gmail.com>
2021-11-09 15:06:53 +0100

    lazy routes

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,17 +1,18 @@
+import { useEffect, Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
-import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { setUser } from "../redux/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import Layout from "./layout/layout";
 import Login from "./login";
-import PollConfig from "./poll-config/poll-config";
-import PollList from "./poll-list/poll-list";
-import PollFill from "./poll-display/poll-fill";
-import PollResults from "./poll-results/poll-results";
-import NotFound from "./misc/not-found";
 import { auth } from "api/firebase";
+
+const PollConfig = lazy(() => import("./poll-config/poll-config"));
+const PollList = lazy(() => import("./poll-list/poll-list"));
+const PollFill = lazy(() => import("./poll-display/poll-fill"));
+const PollResults = lazy(() => import("./poll-results/poll-results"));
+const NotFound = lazy(() => import("./misc/not-found"));
 
 const Routes = () => {
     const history = useHistory();
@@ -33,21 +34,23 @@ const Routes = () => {
 
     return (
         <Layout user={user}>
-            <Switch>
-                {!user && (
-                    <Route path={"/login"} exact strict component={Login} />
-                )}
-                <Route path="/new" exact strict component={PollConfig} />
-                <Route path={"/:id"} exact component={PollFill} />
-                <Route
-                    path={"/:id/answers"}
-                    exact
-                    strict
-                    component={PollResults}
-                />
-                <Route path={"/"} exact component={PollList} />
-                <Route component={NotFound} />
-            </Switch>
+            <Suspense fallback={<div />}>
+                <Switch>
+                    {!user && (
+                        <Route path={"/login"} exact strict component={Login} />
+                    )}
+                    <Route path="/new" exact strict component={PollConfig} />
+                    <Route path={"/:id"} exact component={PollFill} />
+                    <Route
+                        path={"/:id/answers"}
+                        exact
+                        strict
+                        component={PollResults}
+                    />
+                    <Route path={"/"} exact component={PollList} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Suspense>
         </Layout>
     );
 };
